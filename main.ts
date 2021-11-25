3ad584c25553108120098a858d759d77c82d11e9
TuanDao2002 <72557313+TuanDao2002@users.noreply.github.com>
2021-11-25 10:05:59 +0700

    change bad temp speed

--- main.ts
+++ main.ts
@@ -57,7 +57,7 @@ basic.forever(function () {
     if (input.temperature() <= 23) {
         stopSignal = 1
         music.playMelody("G B A G C5 B A B ", 250)
-        basic.showString("BAD TEMP", 85)
+        basic.showString("BAD TEMP", 95)
     } else {
         stopSignal = 0
     }
